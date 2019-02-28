27b2ff70f044e5df8c92541574ba4b81fd0878d6
Summy Singh <summysingh86@gmail.com>
2019-02-28 22:11:27 +0530

    Add files via upload

---
+++ SQLQuery.sql
@@ -0,0 +1,154 @@
+﻿create database sqldb;
+
+create table demo1(Id int identity(1,1),FirstName nvarchar(50),LastName nvarchar(50),Branch nvarchar(50),Contact nvarchar(50),Salary nvarchar(50));
+
+select * from demo1;
+
+insert into demo1 values('summy','singh','cse',9825468669,25000),('sunny','singh','cse',8275622050,125000),('ranjit','kumar','ece',1234567898,26000)
+	,('shubham','gupta','ece',2345615243,27900),('vishal','vaibhav','instrument',9825468669,22000),('supriya','singh','cse',9825468669,20000);
+
+insert into demo1 values('mani','sah','ece',8765432219,30000);
+
+select * from demo1;
+
+select * from demo1 where branch='cse';
+
+select * from demo1 where salary>26000;
+
+select distinct branch from demo1;
+
+select distinct salary from demo1;
+
+select * from demo1 where salary>25000 AND branch='cse' AND LastName='singh';
+
+select * from demo1 where salary>25000 AND branch='cse' OR LastName='singh';
+
+select * from demo1 where salary>25000 OR branch='cse' AND LastName='singh';
+
+select * from demo1 where NOT salary>25000;
+
+select * from demo1 order by salary;
+
+select * from demo1 order by FirstName;
+
+select * from demo1 order by FirstName,LastName;
+
+select * from demo1 order by salary desc;
+
+update demo1 set branch='ece' where id=4;
+
+update demo1 set Contact=9827543524 where id=11;
+
+select * from demo1;
+
+update demo1 set Contact=7827532124 where id=12;
+
+delete from demo1 where id=6;
+
+delete from demo1;
+
+select top 3 * from demo1;
+
+select top 3 * from demo1 where branch='cse';
+
+select MIN(salary) from demo1;
+
+select MAX(salary) from demo1;
+
+select MIN(salary) from demo1 where branch='cse';
+
+select MAX(salary) from demo1 where branch='cse';
+
+select MIN(salary) AS minimumsalary from demo1 where branch='cse';
+
+select count(salary) from demo1;
+
+select count(salary) from demo1 where branch='cse';
+
+select avg(Id) from demo1;
+
+select SUM(Id) from demo1;
+
+select * from demo1 where FirstName Like 's%';
+
+select * from demo1 where branch in ('cse','ece');
+
+select * from demo1 where branch IN (select branch from demo1);
+
+select * from demo1 where salary Between 25000 AND 130000;
+
+select * from demo1 where salary not Between 25000 AND 130000;
+
+select salary as pagad from demo1;
+
+select * from demo1;
+
+create table demo2(Id int identity(7,1),MiddleName nvarchar(50),City nvarchar(50),Age Int, Pincode nvarchar(50),Gender nvarchar(10));
+
+select * from demo2;
+
+insert into demo2 values('Kumar','Daman',22,396210,'male'),('Kumar','Daman',25,396210,'male'),('Garg','Bihar',23,123434,'Female'),('Abcd','Alwar',22,234567,'Male')
+,('Kumari','UP',24,345678,'male'),('Kumar','Jharkhand',22,234567,'Female');
+
+select * from demo2;
+
+select demo1.FirstName , demo2.City 
+From demo1
+INNER JOIN demo2 ON demo1.id=demo2.id;
+
+select demo1.FirstName , demo2.City 
+From demo1
+LEFT JOIN demo2 ON demo1.id=demo2.id;
+
+select demo1.FirstName , demo2.City 
+From demo1
+RIGHT JOIN demo2 ON demo1.id=demo2.id;
+
+select demo1.FirstName , demo2.City 
+From demo1
+FULL OUTER JOIN demo2 ON demo1.id=demo2.id;
+
+select demo1.FirstName , demo2.City 
+From demo1
+CROSS JOIN demo2;
+
+select FirstName from demo1
+UNION
+Select Middlename from demo2
+order by FirstName;
+
+select FirstName from demo1
+UNION ALL
+Select Middlename from demo2;
+
+select Branch from demo1 GROUP BY Branch;
+
+select COUNT(Branch) from demo1 GROUP BY Branch;
+
+select count(branch) from demo1 group by branch having count(branch)>2;
+
+select branch from demo1 group by branch having count(branch)>=3;
+
+select FirstName from demo1 where EXISTS (Select LastName from demo1 where id=demo1.id and salary>25000);
+
+select firstname from demo1 where id = any(select id from demo2 where id=8);
+
+select firstname from demo1 where id = all(select id from demo2 where id=8);
+
+select * into demo3 from demo1;
+
+select * from demo3;
+
+select FirstName, salary, 
+case 
+	WHEN salary>25000 THEN 'Salary is greater than 25k'
+	WHEN salary=25000 THEN 'Salary is 25k'
+	ELSE 'salary is less than 25k'
+End AS SalaryText
+from demo1;
+
+alter table demo1 add email nvarchar(50);
+
+select * from demo1;
+
+alter table demo1 drop column email;
---
+++ myquerys.sql
@@ -0,0 +1,209 @@
+﻿insert into Demo_1(
+Student_ID,
+Student_Name,
+Country,Contact,
+Email,
+Sex,PostalCode,Salary) 
+VALUES
+(1,'Shubham','India',9092752128,'sg1108951@gmail.com','male',201301,'10000');
+(2,'Ranjeet','Pakistan',5654657898,'Ranjeet@gmail.com','male',201302,'5000'),
+(3,'Summy','China',9078564523,'summy@gmail.com','male',201303,'4500'),
+(4,'Mnai','Nepal',9092666128,'mani@gmail.com','male',201308,'6500'),
+(5,'Animesh','USA',5672752128,'animesh@gmail.com','male',201306,'4566'),
+(6,'Vishal','India',9667752128,'Vishal@gmail.com','female',201309,'8900'),
+(7,'Anuj','chile',9092752768,'anuj@gmail.com','female',201304,'2300'),
+(8,'Aman','Pakistan',9092572128,'aman@gmail.com','male',201305,'5500'),
+(9,'Soomy','India',9092123128,'soomy@gmail.com','female',201300,'4300');
+
+select * from Demo_1;
+
+insert into Demo_1(SL,
+Student_ID,
+Student_Name,
+Country,Contact,
+Email,
+Sex,PostalCode,Salary) 
+VALUES
+(1,1,'Shubham','India',9092752128,'shubham@gmail.com','male',201301,'10000');
+
+delete from Demo_1 where Student_ID=1;      
+
+select * from Demo_1;
+
+delete from Demo_1;
+
+select * from Demo_1;
+
+select Country,Sex from Demo_1
+where Sex='female';
+
+select Country,Sex from Demo_1
+where Sex='female' and Country='India';
+
+select Country,Sex from Demo_1
+where Sex='male' and Country='India';
+
+select * from Demo_1
+where Sex='male' and Country='India';
+
+select * from Demo_1
+order by Country;
+
+select * from Demo_1
+order by Country desc;
+
+select * from Demo_1
+order by Student_ID;
+
+
+update Demo_1
+set Student_Name='Shubham_gupta',PostalCode=201307
+where SL=1;
+
+update Demo_1
+set Salary=5600
+where SK
+
+update Demo_1
+set Student_Name='Sonu';
+
+update Demo_1
+set Salary = 4700
+where SL=5;
+
+select * from Demo_1;
+
+select Student_ID,Student_Name, Country, Salary*10 AS AnnualSalary
+from Demo_1
+where Country='India' and Sex='female';
+
+alter table Demo_1
+add Salary VARCHAR(20);
+
+select COUNT(Student_Name)
+from Demo_1
+where Salary > 5000;
+
+select COUNT(Student_Name)
+from Demo_1
+Group by Country;
+
+
+select MAX(Salary)
+from Demo_1
+where Salary=4500
+Group by Country;
+
+select COUNT(Student_Name) as count
+from Demo_1
+Group by Country
+Having MAX(Salary) > 5000;
+
+select Email, Country
+from Demo_1
+Group by Email, Country
+Having MAX(Salary) > 2000;
+
+
+select Email from Demo_1
+where Salary > (select top 1 salary from Demo_1
+where Salary > 5000);
+
+delete from Demo_1
+where Contact=9092752768;
+
+select Email,City from Demo_1;
+
+Alter table Demo_1
+add City VARCHAR(20);
+
+select * from Demo_2;
+
+insert into Demo_2 
+(State,Address,Designation)
+values 
+('Rajasthan','NEB','DEVELOPER'),
+('TamilNadu','VIT','DEVELOPER'),
+('Kerala','abc','TESTER'),
+('Haryana','def','SERVICE DESK'),
+('Rajasthan','qwerty','TESTER');
+
+delete from Demo_2
+where SL=2;
+
+
+
+///////JOINS
+
+
+SELECT * FROM Demo_1 D1 INNER JOIN Demo_2 D2 
+ON D1.SL=D2.SL;
+
+SELECT * FROM Demo_1 D1 LEFT JOIN Demo_2 D2
+ON D1.SL=D2.SL;
+
+SELECT * FROM Demo_1 D1 LEFT OUTER JOIN Demo_2 D2
+ON D1.SL=D2.SL;
+
+
+
+select SL from Demo_1
+union 
+select SL from Demo_2;
+
+select SL from Demo_1
+union
+select SL from Demo_2;
+
+select SL from Demo_1
+union all
+select SL from Demo_2;
+
+select COUNT(Student_Name)
+from Demo_1
+Group by Sex;
+
+select * from Demo_1;
+
+select * from Demo_1
+where Salary between 2000 and 7000;
+
+select * from Demo_1
+where Country in('India','Pakistan');
+
+select * from Demo_1
+where Student_ID between 3 and 6;
+
+select MAX(Salary) from Demo_1
+group by Country;
+
+create table Demmo_3
+	(
+	ID int PRIMARY KEY,
+	FirstName varchar(50),
+    LastName varchar(50) NOT NULL,
+    Age int
+	);
+
+select * from Demo_3;
+
+insert into Demo_3(
+ID,FirstName,LastName,Age)
+values
+(1,'Shubham','Gupta',23),
+(2,'Summy','Singh',22),
+(3,'Rbajeet','Kumar',21),
+(4,'Mani','Sah',23),
+(5,'Diwid','singh',22);
+
+create table Demo_4
+(
+DepID  int Primary key,
+Name   nvarchar(20),
+Address nvarchar(20) unique,
+Salary  nvarchar(20),
+ID int foreign key REFERENCES Demo_3(ID)
+);
+
+
+select * from Demo_4;
